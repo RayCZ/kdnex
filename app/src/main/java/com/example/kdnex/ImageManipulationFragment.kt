2aa4bbb5db0b00016a0e7147881880c2497159a1
zuri-yi <ka780906@gmail.com>
2019-05-15 17:30:43 +0800

    init drawer fragment .kt and layout.xml

---
+++ app/src/main/java/com/example/kdnex/ImageManipulationFragment.kt
@@ -0,0 +1,20 @@
+package com.example.kdnex
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+
+class ImageManipulationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_calculator, container, false)
+    }
+
+
+}
--- app/src/main/java/com/example/kdnex/MainActivity.kt
+++ app/src/main/java/com/example/kdnex/MainActivity.kt
@@ -1,12 +1,86 @@
 package com.example.kdnex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private var drawer: DrawerLayout? = null
+    private var toolbar: Toolbar? = null
+    private var left_navi: NavigationView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        drawer = findViewById(R.id.drawer)
+        toolbar = findViewById(R.id.toolbar)
+        left_navi = findViewById(R.id.left_navi)
+
+        initDrawer()
+
+
+    }
+
+    fun initDrawer() {
+        setSupportActionBar(toolbar)
+        toolbar?.setNavigationOnClickListener {
+            drawer?.openDrawer(GravityCompat.START)
+        }
+
+        val drawerToggle = ActionBarDrawerToggle(this@MainActivity, drawer, toolbar, R.string.open, R.string.close)
+        drawer?.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        left_navi?.setNavigationItemSelectedListener { menuItem ->
+            drawer?.closeDrawer(GravityCompat.START)
+            when (menuItem.itemId) {
+                R.id.navCalculator -> {
+                    replaceFragment(CalculatorFragment())
+                    true
+                }
+
+                R.id.navcCanvas -> {
+                    replaceFragment(CanvasFragment())
+                    true
+                }
+
+                R.id.navClock -> {
+                    replaceFragment(ClockFragment())
+                    true
+                }
+
+                R.id.navImage -> {
+                    replaceFragment(ImageManipulationFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.main_layout, fragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        drawer?.let {
+            if (it.isDrawerOpen(GravityCompat.START)) {
+                it.closeDrawer(GravityCompat.START)
+            } else {
+                super.onBackPressed()
+            }
+        }
+
+
     }
 }
