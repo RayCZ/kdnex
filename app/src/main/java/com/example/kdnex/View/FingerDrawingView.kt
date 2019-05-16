f0f465dd978584c9c5ae0cb3a2716fc50d3d9fa7
zuri-yi <ka780906@gmail.com>
2019-05-16 20:16:54 +0800

    finish FingerDrawingView

--- app/src/main/java/com/example/kdnex/View/FingerDrawingView.kt
+++ app/src/main/java/com/example/kdnex/View/FingerDrawingView.kt
@@ -1,10 +1,7 @@
 package com.example.kdnex.View
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
@@ -12,50 +9,72 @@ import android.view.View
 class FingerDrawingView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-
     private val path: Path
+    private val pathPaint: Paint
     private val backgroundPaint: Paint
 
 
-    private var canvas: Canvas? = null
+    private var backdroundCanvas: Canvas? = null
     private var backgroundBitmap: Bitmap? = null
 
 
-    private var xPosition: Float = 0f
-    private var yPosition: Float = 0f
-
     init {
         path = Path()
+        pathPaint = Paint().apply {
+            isAntiAlias = true
+            isDither = true
+            color = Color.RED
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+            strokeWidth = 12.toFloat()
+        }
+
         backgroundPaint = Paint(Paint.DITHER_FLAG)
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         backgroundBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
-        backgroundBitmap?.let { canvas = Canvas(it) }
+        backgroundBitmap?.let { backdroundCanvas = Canvas(it) }
 
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        backgroundBitmap?.let { canvas?.drawBitmap(it, 0f, 0f, backgroundPaint) }
-
+        backgroundBitmap?.let {
+            canvas?.drawBitmap(it, 0f, 0f, backgroundPaint)
+            canvas?.drawPath(path, pathPaint)
+        }
     }
+
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         when (event?.action) {
             MotionEvent.ACTION_DOWN -> {
-                path.reset()
                 path.moveTo(event.x, event.y)
-                xPosition = event.x
-                yPosition = event.y
+                invalidate()
             }
             MotionEvent.ACTION_MOVE -> {
+                path.lineTo(event.x, event.y)
+                backdroundCanvas?.drawPath(path, pathPaint)
+                invalidate()
             }
             MotionEvent.ACTION_UP -> {
+                path.reset()
+                performClick()
+                invalidate()
             }
         }
 
-        return super.onTouchEvent(event)
+        return true
+    }
+
+    override fun performClick(): Boolean {
+        return super.performClick()
+    }
+
+    companion object {
+        const val TOUCH_TOLERANCE = 4
     }
 }
